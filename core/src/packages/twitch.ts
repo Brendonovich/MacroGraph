c96dcef535d03531e50afe3577d6c26eb0f926f7
jdude700 <info@jdude.tv>
2023-04-29 13:46:32 +1000

    Adding in more twitch API nodes

--- core/src/packages/twitch.ts
+++ core/src/packages/twitch.ts
@@ -213,7 +213,12 @@ pkg.createEventSchema({
         });
         t.dataOutput({
             id: "modId",
-            name: "Mod Who Banned",
+            name: "Mod Who Banned ID",
+            type: types.string()
+        });
+        t.dataOutput({
+            id: "modName",
+            name: "Mod Who Banned Name",
             type: types.string()
         });
         t.dataOutput({
@@ -246,7 +251,8 @@ pkg.createEventSchema({
         console.log(data);
         ctx.setOutput("channelId", data.event.broadcaster_user_id);
         ctx.setOutput("channelName", data.event.broadcaster_user_login);
-        ctx.setOutput("modId", data.event.moderator_user_login);
+        ctx.setOutput("modId", data.event.moderator_user_id);
+        ctx.setOutput("modName", data.event.moderator_user_login);
         ctx.setOutput("bannedUserID", data.event.user_id);
         ctx.setOutput("bannedUserName", data.event.user_login);
         ctx.setOutput("reason", data.event.reason);
@@ -255,6 +261,539 @@ pkg.createEventSchema({
         ctx.exec("exec");
     }
 });
+
+pkg.createEventSchema({
+    name: "Unban",
+    event: "channel.unban",
+    generateIO: (t) => {
+        t.execOutput({
+            id: "exec",
+            name: "",
+        });
+        t.dataOutput({
+            id: "userId",
+            name: "userID",
+            type: types.string()
+        });
+        t.dataOutput({
+            id: "userLogin",
+            name: "Username",
+            type: types.string()
+        });
+        t.dataOutput({
+            id: "modName",
+            name: "Mod Who unbanned name",
+            type: types.string()
+        });
+        t.dataOutput({
+            id: "modId",
+            name: "Mod Who unbanned Id",
+            type: types.string()
+        });
+    },
+    run({ ctx, data}) {
+        console.log(data);
+        ctx.setOutput("userId", data.event.from_broadcaster_user_id);
+        ctx.setOutput("userLogin", data.event.from_broadcaster_user_login);
+        ctx.setOutput("modName", data.event.moderator_user_login);
+        ctx.setOutput("modId", data.event.moderator_user_id);
+        ctx.exec("exec");
+    }
+});
+
+pkg.createEventSchema({
+    name: "Moderator Add",
+    event: "channel.moderator.add",
+    generateIO: (t) => {
+        t.execOutput({
+            id: "exec",
+            name: "",
+        });
+        t.dataOutput({
+            id: "userId",
+            name: "userID",
+            type: types.string()
+        });
+        t.dataOutput({
+            id: "userLogin",
+            name: "Username",
+            type: types.string()
+        });
+    },
+    run({ ctx, data}) {
+        console.log(data);
+        ctx.setOutput("userId", data.event.user_id);
+        ctx.setOutput("userLogin", data.event.user_login);
+        ctx.exec("exec");
+    }
+});
+
+pkg.createEventSchema({
+    name: "Moderator Remove",
+    event: "channel.moderator.remove",
+    generateIO: (t) => {
+        t.execOutput({
+            id: "exec",
+            name: "",
+        });
+        t.dataOutput({
+            id: "userId",
+            name: "userID",
+            type: types.string()
+        });
+        t.dataOutput({
+            id: "userLogin",
+            name: "Username",
+            type: types.string()
+        });
+    },
+    run({ ctx, data}) {
+        console.log(data);
+        ctx.setOutput("userId", data.event.user_id);
+        ctx.setOutput("userLogin", data.event.user_login);
+        ctx.exec("exec");
+    }
+});
+
+pkg.createEventSchema({
+    name: "Channel Point Reward Add",
+    event: "channel.channel_points_custom_reward.add",
+    generateIO: (t) => {
+        t.execOutput({
+            id: "exec",
+            name: "",
+        });
+        t.dataOutput({
+            id: "id",
+            name: "ID",
+            type: types.string()
+        });
+        t.dataOutput({
+            id: "enabled",
+            name: "Enabled",
+            type: types.bool()
+        });
+        t.dataOutput({
+            id: "paused",
+            name: "Paused",
+            type: types.bool()
+        });
+        t.dataOutput({
+            id: "inStock",
+            name: "In Stock",
+            type: types.bool()
+        });
+        t.dataOutput({
+            id: "title",
+            name: "Title",
+            type: types.string()
+        });
+        t.dataOutput({
+            id: "cost",
+            name: "Cost",
+            type: types.int()
+        });
+        t.dataOutput({
+            id: "prompt",
+            name: "Prompt",
+            type: types.string()
+        });
+        t.dataOutput({
+            id: "inputRequired",
+            name: "Input Required",
+            type: types.bool()
+        });
+        t.dataOutput({
+            id: "skipQueue",
+            name: "Skip Request Queue",
+            type: types.bool()
+        });
+        t.dataOutput({
+            id: "cooldownExpire",
+            name: "Cooldown Expire Timestamp",
+            type: types.string()
+        });
+        t.dataOutput({
+            id: "redemptTotalStream",
+            name: "Current Stream Total Redemptions",
+            type: types.int()
+        });
+        t.dataOutput({
+            id: "maxPerStreamEnabled",
+            name: "Max per Stream",
+            type: types.bool()
+        });
+        t.dataOutput({
+            id: "maxPerStreamValue",
+            name: "Max Per Stream Value",
+            type: types.int()
+        });
+        t.dataOutput({
+            id: "maxUserPerStream",
+            name: "Max User Per Stream Enabled",
+            type: types.bool()
+        });
+        t.dataOutput({
+            id: "maxUserPerStreamValue",
+            name: "Max User Per Stream Value",
+            type: types.int()
+        });
+        t.dataOutput({
+            id: "globalCooldown",
+            name: "Global Cooldown",
+            type: types.bool()
+        });
+        t.dataOutput({
+            id: "globalCooldownValue",
+            name: "Global Cooldown Value",
+            type: types.int()
+        });
+        t.dataOutput({
+            id: "backgroundColor",
+            name: "Background Color",
+            type: types.string()
+        });
+    },
+    run({ ctx, data}) {
+        console.log(data);
+        ctx.setOutput("id", data.event.id);
+        ctx.setOutput("enabled", data.event.is_enabled);
+        ctx.setOutput("paused", data.event.is_paused);
+        ctx.setOutput("inStock", data.event.is_in_stock);
+        ctx.setOutput("title", data.event.title);
+        ctx.setOutput("cost", data.event.cost);
+        ctx.setOutput("prompt", data.event.prompt);
+        ctx.setOutput("inputRequired", data.event.is_user_input_required);
+        ctx.setOutput("skipQueue", data.event.should_redemptions_skip_request_queue);
+        ctx.setOutput("cooldownExpire", data.event.cooldown_expires_at);
+        ctx.setOutput("redemptTotalStream", data.event.redemptions_redeemed_current_stream);
+        ctx.setOutput("maxPerStreamEnabled", data.event.max_per_stream.is_enabled);
+        ctx.setOutput("maxPerStreamValue", data.event.max_per_stream.value);
+        ctx.setOutput("maxUserPerStream", data.event.max_per_user_per_stream.is_enabled);
+        ctx.setOutput("maxUserPerStreamValue", data.event.max_per_user_per_stream.value);
+        ctx.setOutput("globalCooldown", data.event.global_cooldown.is_enabled);
+        ctx.setOutput("globalCooldownValue", data.event.global_cooldown.seconds);
+        ctx.setOutput("backgroundColor", data.event.background_color);
+        ctx.exec("exec");
+    }
+});
+
+pkg.createEventSchema({
+    name: "Channel Updated",
+    event: "channel.update",
+    generateIO: (t) => {
+        t.execOutput({
+            id: "exec",
+            name: "",
+        });
+        t.dataOutput({
+            id: "channelId",
+            name: "Channel ID",
+            type: types.string()
+        });
+        t.dataOutput({
+            id: "channelName",
+            name: "Channel Name",
+            type: types.string()
+        });
+        t.dataOutput({
+            id: "title",
+            name: "Title",
+            type: types.string()
+        });
+        t.dataOutput({
+            id: "categoryId",
+            name: "Category Id",
+            type: types.string()
+        });
+        t.dataOutput({
+            id: "categoryName",
+            name: "Category Name",
+            type: types.string()
+        });
+        t.dataOutput({
+            id: "mature",
+            name: "Mature",
+            type: types.string()
+        });
+    },
+    run({ ctx, data}) {
+        console.log(data);
+        ctx.setOutput("channelId", data.event.broadcaster_user_id);
+        ctx.setOutput("channelName", data.event.broadcaster_user_login);
+        ctx.setOutput("title", data.event.title);
+        ctx.setOutput("categoryId", data.event.category_id);
+        ctx.setOutput("categoryName", data.event.category_name);
+        ctx.setOutput("mature", data.event.is_mature);
+        ctx.exec("exec");
+    }
+});
+
+pkg.createEventSchema({
+    name: "Channel Subscribe",
+    event: "channel.subscribe",
+    generateIO: (t) => {
+        t.execOutput({
+            id: "exec",
+            name: "",
+        });
+        t.dataOutput({
+            id: "userId",
+            name: "userID",
+            type: types.string()
+        });
+        t.dataOutput({
+            id: "userLogin",
+            name: "Username",
+            type: types.string()
+        });
+        t.dataOutput({
+            id: "tier",
+            name: "Tier",
+            type: types.string()
+        });
+        t.dataOutput({
+            id: "isGift",
+            name: "Gifted",
+            type: types.bool()
+        });
+    },
+    run({ ctx, data}) {
+        console.log(data);
+        ctx.setOutput("userId", data.event.user_id);
+        ctx.setOutput("userLogin", data.event.user_login);
+        ctx.setOutput("tier", data.event.tier);
+        ctx.setOutput("isGift", data.event.is_gift);
+        ctx.exec("exec");
+    }
+});
+
+pkg.createEventSchema({
+    name: "Channel Subscribe End",
+    event: "channel.subscription.end",
+    generateIO: (t) => {
+        t.execOutput({
+            id: "exec",
+            name: "",
+        });
+        t.dataOutput({
+            id: "userId",
+            name: "userID",
+            type: types.string()
+        });
+        t.dataOutput({
+            id: "userLogin",
+            name: "Username",
+            type: types.string()
+        });
+        t.dataOutput({
+            id: "tier",
+            name: "Tier",
+            type: types.string()
+        });
+        t.dataOutput({
+            id: "isGift",
+            name: "Gifted",
+            type: types.bool()
+        });
+    },
+    run({ ctx, data}) {
+        console.log(data);
+        ctx.setOutput("userId", data.event.user_id);
+        ctx.setOutput("userLogin", data.event.user_login);
+        ctx.setOutput("tier", data.event.tier);
+        ctx.setOutput("isGift", data.event.is_gift);
+        ctx.exec("exec");
+    }
+});
+
+pkg.createEventSchema({
+    name: "Subscription Gift",
+    event: "channel.subscription.gift",
+    generateIO: (t) => {
+        t.execOutput({
+            id: "exec",
+            name: "",
+        });
+        t.dataOutput({
+            id: "userId",
+            name: "userID",
+            type: types.string()
+        });
+        t.dataOutput({
+            id: "userLogin",
+            name: "Username",
+            type: types.string()
+        });
+        t.dataOutput({
+            id: "tier",
+            name: "Tier",
+            type: types.string()
+        });
+        t.dataOutput({
+            id: "total",
+            name: "Total",
+            type: types.int()
+        });
+        t.dataOutput({
+            id: "cumulative",
+            name: "Cumulative Total",
+            type: types.int()
+        });
+        t.dataOutput({
+            id: "anonymous",
+            name: "Anonymous",
+            type: types.bool()
+        });
+    },
+    run({ ctx, data}) {
+        console.log(data);
+        ctx.setOutput("userId", data.event.user_id);
+        ctx.setOutput("userLogin", data.event.user_login);
+        ctx.setOutput("tier", data.event.tier);
+        ctx.setOutput("total", data.event.total);
+        ctx.setOutput("cumulative", data.event.cumulative_total);
+        ctx.setOutput("anonymous", data.event.is_anonymous);
+        ctx.exec("exec");
+    }
+});
+
+pkg.createEventSchema({
+    name: "Subscription Message",
+    event: "channel.subscription.message",
+    generateIO: (t) => {
+        t.execOutput({
+            id: "exec",
+            name: "",
+        });
+        t.dataOutput({
+            id: "userId",
+            name: "userID",
+            type: types.string()
+        });
+        t.dataOutput({
+            id: "userLogin",
+            name: "Username",
+            type: types.string()
+        });
+        t.dataOutput({
+            id: "tier",
+            name: "Tier",
+            type: types.string()
+        });
+        t.dataOutput({
+            id: "message",
+            name: "Message",
+            type: types.string()
+        });
+        t.dataOutput({
+            id: "streak",
+            name: "Streak Months",
+            type: types.int()
+        });
+        t.dataOutput({
+            id: "cumulative",
+            name: "Cumulative Months",
+            type: types.int()
+        });
+        t.dataOutput({
+            id: "duration",
+            name: "Duration Months",
+            type: types.bool()
+        });
+    },
+    run({ ctx, data}) {
+        console.log(data);
+        ctx.setOutput("userId", data.event.user_id);
+        ctx.setOutput("userLogin", data.event.user_login);
+        ctx.setOutput("tier", data.event.tier);
+        ctx.setOutput("message", data.event.message.text);
+        ctx.setOutput("cumulative", data.event.cumulative_months);
+        ctx.setOutput("streak", data.event.streak_months);
+        ctx.setOutput("duration", data.event.duration_months);
+        ctx.exec("exec");
+    }
+});
+
+pkg.createEventSchema({
+    name: "Cheers",
+    event: "channel.cheer",
+    generateIO: (t) => {
+        t.execOutput({
+            id: "exec",
+            name: "",
+        });
+        t.dataOutput({
+            id: "userId",
+            name: "userID",
+            type: types.string()
+        });
+        t.dataOutput({
+            id: "userLogin",
+            name: "Username",
+            type: types.string()
+        });
+        t.dataOutput({
+            id: "anonymous",
+            name: "Anonymous",
+            type: types.bool()
+        });
+        t.dataOutput({
+            id: "message",
+            name: "Message",
+            type: types.string()
+        });
+        t.dataOutput({
+            id: "bits",
+            name: "Bits",
+            type: types.int()
+        });
+    },
+    run({ ctx, data}) {
+        console.log(data);
+        ctx.setOutput("userId", data.event.user_id);
+        ctx.setOutput("userLogin", data.event.user_login);
+        ctx.setOutput("anonymous", data.event.is_anonymous);
+        ctx.setOutput("message", data.event.message.text);
+        ctx.setOutput("bits", data.event.bits);
+        ctx.exec("exec");
+    }
+});
+
+pkg.createEventSchema({
+    name: "Raid",
+    event: "channel.raid",
+    generateIO: (t) => {
+        t.execOutput({
+            id: "exec",
+            name: "",
+        });
+        t.dataOutput({
+            id: "userId",
+            name: "userID",
+            type: types.string()
+        });
+        t.dataOutput({
+            id: "userLogin",
+            name: "Username",
+            type: types.string()
+        });
+        t.dataOutput({
+            id: "viewers",
+            name: "Viewers",
+            type: types.int()
+        });
+    },
+    run({ ctx, data}) {
+        console.log(data);
+        ctx.setOutput("userId", data.event.from_broadcaster_user_id);
+        ctx.setOutput("userLogin", data.event.from_broadcaster_user_login);
+        ctx.setOutput("viewers", data.event.viewers);
+        ctx.exec("exec");
+    }
+});
+
+
 
 pkg.createEventSchema({
     name: "User Followed",
@@ -337,14 +876,9 @@ pkg.createNonEventSchema({
             name: "Message",
             type: types.string(),
         });
-        t.dataInput({
-            id: "channel",
-            name: "Channel",
-            type: types.string(),
-        });
     },
     run({ ctx }) {
-        Client.say(ctx.getInput<string>("channel").toLowerCase(), ctx.getInput("message"));
+        Client.say( username , ctx.getInput("message"));
     }
 });
 
