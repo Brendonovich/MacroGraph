e4a24d01110d00dea652d35e6d8783a5c672a42d
lclc98 <lclc98@lclc98.com>
2023-04-29 22:33:40 +1000

    Initial work for websocket

--- app/src-tauri/src/main.rs
+++ app/src-tauri/src/main.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use websocket;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -8,8 +10,10 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
+    tauri::async_runtime::spawn(websocket::start_server());
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
+
