fd2288db78b8a81a1234c392e9aa7bdcf84e4963
SkechyWolf <37350695+SkechyWolf@users.noreply.github.com>
2023-04-29 21:37:29 -0700

    Add Float to String

--- core/src/packages/utils.ts
+++ core/src/packages/utils.ts
@@ -189,6 +189,24 @@ pkg.createNonEventSchema({
 });
 
 pkg.createNonEventSchema({
+    name: "Float to String",
+    variant: "Pure",
+    run({ ctx }) {
+        ctx.setOutput("string", ctx.getInput<number>("float").toString());
+    },
+    generateIO(builder) {
+        builder.dataInput({
+        id: "float",
+        type: types.float(),
+        });
+        builder.dataOutput({
+        id: "string",
+        type: types.string(),
+        });
+    },
+});
+
+pkg.createNonEventSchema({
   name: "Bool to String",
   variant: "Pure",
   run({ ctx }) {
