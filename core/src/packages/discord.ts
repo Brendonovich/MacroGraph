42f3ce29ad78e6a4b3830974d33475cbef32b878
Brendan Allan <brendonovich@outlook.com>
2023-05-05 13:22:24 +0800

    better discord api (#59)

--- core/src/packages/discord.ts
+++ core/src/packages/discord.ts
@@ -1,182 +1,261 @@
 import { core } from "../models";
 import { types } from "../types";
-// import { fetch, Body } from "tauri-plugin-http-api";
-
 
 const pkg = core.createPackage<any>({ name: "Discord" });
 
-export const LSTokenName = "discordBotToken"
+export const LSTokenName = "discordBotToken";
 const Token = localStorage.getItem(LSTokenName);
 console.log(Token);
 
 const ws = new WebSocket("wss://gateway.discord.gg/?v=6&encoding=json");
 
-const fetchTypes = {
-    POST: 'POST',
-    GET: 'GET',
-    PATCH: 'PATCH'
-}
-
 if (Token) {
+  var interval = 0;
+  var token = Token;
 
-    var interval = 0;
-    var token = Token;
+  const payload = {
+    op: 2,
+    d: {
+      token: token,
+      intents: 33280,
+      properties: {
+        $os: "linux",
+        $browser: "chrome",
+        $device: "chrome",
+      },
+    },
+  };
 
-    const payload = {
-        op: 2,
-        d: {
-            token: token,
-            intents: 33280,
-            properties: {
-                $os: "linux",
-                $browser: "chrome",
-                $device: "chrome",
-            },
-        },
+  ws.addEventListener("open", function open(x) {
+    ws.send(JSON.stringify(payload));
+  });
+
+  let seq;
+  ws.addEventListener("message", function incoming(data) {
+    let x = data.data;
+    let payload = JSON.parse(x);
+
+    const { t, event, op, d, s } = payload;
+    seq = s;
+    switch (op) {
+      // OPCODE 10 GIVES the HEARTBEAT INTERVAL, SO YOU CAN KEEP THE CONNECTION ALIVE
+      case 10:
+        const { heartbeat_interval } = d;
+        ws.send(JSON.stringify({ op: 1, d: null }));
+        setInterval(() => {
+          ws.send(JSON.stringify({ op: 1, d: seq }));
+        }, heartbeat_interval);
+
+        break;
+    }
+
+    switch (t) {
+      // IF MESSAGE IS CREATED, IT WILL LOG IN THE CONSOLE
+      case "MESSAGE_CREATE":
+        console.log(d.type);
+        if (d.type !== 0) return;
+        pkg.emitEvent({
+          name: "discordMessage",
+          data: d,
+        });
+    }
+  });
+}
+
+type Endpoint = ReturnType<typeof createEndpoint>;
+type HTTPMethod = "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
+
+interface EndpointArgs {
+  path: string;
+  extend?: Endpoint;
+}
+
+function createEndpoint({ path, extend }: EndpointArgs) {
+  if (extend) path = `${extend.path}{path}`;
+
+  const createFetcher = (method: HTTPMethod) => (args?: { body?: string }) =>
+    fetch(path, {
+      method,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bot ${Token}`,
+      },
+      ...args,
+    });
+
+  return {
+    path,
+    get: createFetcher("GET"),
+    post: createFetcher("POST"),
+    put: createFetcher("PUT"),
+    patch: createFetcher("PATCH"),
+    delete: createFetcher("DELETE"),
+  };
+}
+
+const apiEndpoint = createEndpoint({ path: "https://discordapp.com/api/v10" });
+
+const discordApi = {
+  channels(id: string) {
+    const channelsEndpoint = createEndpoint({
+      path: `/channels/${id}`,
+      extend: apiEndpoint,
+    });
+
+    return {
+      messages: createEndpoint({
+        extend: channelsEndpoint,
+        path: `/messages`,
+      }),
     };
-
-    ws.addEventListener("open", function open(x) {
-        ws.send(JSON.stringify(payload));
-    });
-
-    let seq;
-    ws.addEventListener("message", function incoming(data) {
-        let x = data.data;
-        let payload = JSON.parse(x);
-
-        const { t, event, op, d, s } = payload;
-        seq = s;
-        switch (op) {
-            // OPCODE 10 GIVES the HEARTBEAT INTERVAL, SO YOU CAN KEEP THE CONNECTION ALIVE
-            case 10:
-                const { heartbeat_interval } = d;
-                ws.send(JSON.stringify({ op: 1, d: null }));
-                setInterval(() => {
-                    ws.send(JSON.stringify({ op: 1, d: seq }));
-                }, heartbeat_interval);
-
-                break;
-        }
-
-        switch (t) {
-            // IF MESSAGE IS CREATED, IT WILL LOG IN THE CONSOLE
-            case "MESSAGE_CREATE":
-                console.log(d.type);
-                if (d.type !== 0) return;
-                pkg.emitEvent({
-                    name: "discordMessage",
-                    data: d,
-                })
-
-        }
-    });
-
-}
-
-async function SendMessage(urlEnd: string, body: any, type: string) {
-    const URL = `https://discordapp.com/api/v10/channels/${urlEnd}`
-    const response = await fetch(URL, {
-        method: type,
-        body: type == fetchTypes.POST || type == fetchTypes.PATCH ? JSON.stringify(body) : null,
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bot ${Token}`,
-        },
-    });
-    return response;
-}
+  },
+  users(id: string) {
+    return createEndpoint({ path: `/users/${id}`, extend: apiEndpoint });
+  },
+};
 
 pkg.createNonEventSchema({
-    name: "Send Discord Message",
-    variant: "Exec",
-    generateIO: (t) => {
-        t.execInput({
-            id: "exec",
-        });
-        t.dataInput({
-            id: "message",
-            name: "Message",
-            type: types.string()
-        });
-        t.dataInput({
-            id: "channel",
-            name: "Channel ID",
-            type: types.string()
-        })
-    },
-    async run({ ctx }) {
-        const urlEnd = `${ctx.getInput("channel")}/messages`;
-        await SendMessage(urlEnd, { content: ctx.getInput("message") }, fetchTypes.POST);
-        ctx.exec("exec");
-    }
-})
+  name: "Send Discord Message",
+  variant: "Exec",
+  generateIO: (t) => {
+    t.execInput({
+      id: "exec",
+    });
+    t.dataInput({
+      id: "message",
+      name: "Message",
+      type: types.string(),
+    });
+    t.dataInput({
+      id: "channel",
+      name: "Channel ID",
+      type: types.string(),
+    });
+  },
+  async run({ ctx }) {
+    await discordApi.channels(ctx.getInput("channel")).messages.post({
+      body: JSON.stringify({ content: ctx.getInput("message") }),
+    });
 
-pkg.createNonEventSchema({
-    name: "Get Discord User",
-    variant: "Exec",
-    generateIO: (t) => {
-        t.execInput({
-            id: "exec",
-        });
-        t.dataInput({
-            id: "userId",
-            name: "User ID",
-            type: types.int()
-        });
-    },
-    async run({ ctx }) {
-        const urlEnd = `users/${ctx.getInput("userId")}`;
-        const response = await SendMessage(urlEnd, {}, fetchTypes.GET);
-        console.log(response);
-    }
-})
+    ctx.exec("exec");
+  },
+});
 
 pkg.createEventSchema({
-    name: "Discord Message",
-    event: "discordMessage",
-    generateIO: (t) => {
-        t.execOutput({
-            id: "exec",
-        });
-        t.dataOutput({
-            id: "message",
-            name: "Message",
-            type: types.string(),
-        });
-        t.dataOutput({
-            id: "channelId",
-            name: "Channel ID",
-            type: types.string(),
-        });
-        t.dataOutput({
-            id: "username",
-            name: "Username",
-            type: types.string(),
-        });
-        t.dataOutput({
-            id: "userId",
-            name: "User ID",
-            type: types.string(),
-        });
-        t.dataOutput({
-            id: "nickname",
-            name: "Nickname",
-            type: types.string(),
-        });
-        t.dataOutput({
-            id: "roles",
-            name: "Roles",
-            type: types.list(types.string()),
-        });
-    },
-    run({ ctx, data }) {
-        console.log(data);
-        ctx.setOutput("message", data.content);
-        ctx.setOutput("channelId", data.channel_id);
-        ctx.setOutput("username", data.author.username);
-        ctx.setOutput("userId", data.author.id);
-        ctx.setOutput("nickname", data.member.nick);
-        ctx.setOutput("roles", data.member.roles);
-        ctx.exec("exec");
-    }
+  name: "Discord Message",
+  event: "discordMessage",
+  generateIO: (t) => {
+    t.execOutput({
+      id: "exec",
+    });
+    t.dataOutput({
+      id: "message",
+      name: "Message",
+      type: types.string(),
+    });
+    t.dataOutput({
+      id: "channelId",
+      name: "Channel ID",
+      type: types.string(),
+    });
+    t.dataOutput({
+      id: "username",
+      name: "Username",
+      type: types.string(),
+    });
+    t.dataOutput({
+      id: "userId",
+      name: "User ID",
+      type: types.string(),
+    });
+    t.dataOutput({
+      id: "nickname",
+      name: "Nickname",
+      type: types.string(),
+    });
+    t.dataOutput({
+      id: "roles",
+      name: "Roles",
+      type: types.list(types.string()),
+    });
+  },
+  run({ ctx, data }) {
+    console.log(data);
+    ctx.setOutput("message", data.content);
+    ctx.setOutput("channelId", data.channel_id);
+    ctx.setOutput("username", data.author.username);
+    ctx.setOutput("userId", data.author.id);
+    ctx.setOutput("nickname", data.member.nick);
+    ctx.setOutput("roles", data.member.roles);
+    ctx.exec("exec");
+  },
+});
+
+pkg.createNonEventSchema({
+  name: "Get Discord User",
+  variant: "Exec",
+  generateIO: (t) => {
+    t.execInput({
+      id: "exec",
+    });
+    t.dataInput({
+      id: "userId",
+      name: "User ID",
+      type: types.int(),
+    });
+  },
+  async run({ ctx }) {
+    const response = await discordApi.users(ctx.getInput("userId")).get();
+    console.log(response);
+  },
+});
+
+pkg.createEventSchema({
+  name: "Discord Message",
+  event: "discordMessage",
+  generateIO: (t) => {
+    t.execOutput({
+      id: "exec",
+    });
+    t.dataOutput({
+      id: "message",
+      name: "Message",
+      type: types.string(),
+    });
+    t.dataOutput({
+      id: "channelId",
+      name: "Channel ID",
+      type: types.string(),
+    });
+    t.dataOutput({
+      id: "username",
+      name: "Username",
+      type: types.string(),
+    });
+    t.dataOutput({
+      id: "userId",
+      name: "User ID",
+      type: types.string(),
+    });
+    t.dataOutput({
+      id: "nickname",
+      name: "Nickname",
+      type: types.string(),
+    });
+    t.dataOutput({
+      id: "roles",
+      name: "Roles",
+      type: types.list(types.string()),
+    });
+  },
+  run({ ctx, data }) {
+    console.log(data);
+    ctx.setOutput("message", data.content);
+    ctx.setOutput("channelId", data.channel_id);
+    ctx.setOutput("username", data.author.username);
+    ctx.setOutput("userId", data.author.id);
+    ctx.setOutput("nickname", data.member.nick);
+    ctx.setOutput("roles", data.member.roles);
+    ctx.exec("exec");
+  },
 });
