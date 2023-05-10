94701edff9a4ae3b883eff9b4750964ffeec01b7
jdude700 <info@jdude.tv>
2023-05-11 09:27:06 +1000

    Added localstorage nodes.

---
+++ core/src/packages/localStorage.ts
@@ -0,0 +1,54 @@
+import { core } from "../models";
+import { types } from "../types";
+
+
+const pkg = core.createPackage({
+    name: "Localstorage",
+});
+
+pkg.createNonEventSchema({
+    name: "Set Data",
+    variant: "Exec",
+    generateIO: (t) => {
+        t.dataInput({
+            id: "key",
+            name: "Key",
+            type: types.string(),
+        });
+        t.dataInput({
+            id: "value",
+            name: "Value",
+            type: types.string(),
+        });
+    },
+    run({ ctx }) {
+        localStorage.setItem(`value-${ctx.getInput("key")}`, ctx.getInput("value"));
+    }
+})
+
+pkg.createNonEventSchema({
+    name: "Get Data",
+    variant: "Pure",
+    generateIO: (t) => {
+        t.dataInput({
+            id: "key",
+            name: "Key",
+            type: types.string(),
+        });
+        t.dataOutput({
+            id: "exist",
+            name: "Exist",
+            type: types.bool(),
+        });
+        t.dataOutput({
+            id: "output",
+            name: "Data",
+            type: types.string(),
+        });
+    },
+    run({ ctx }) {
+        let data = localStorage.getItem(`value-${ctx.getInput("key")}`);
+        ctx.setOutput("exist", data !== null);
+        if (data !== null) ctx.setOutput("output", data);
+    }
+});
