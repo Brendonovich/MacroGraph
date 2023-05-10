be13654210f4eb3e28eff0e286a81e535762f2a2
Brendan Allan <brendonovich@outlook.com>
2023-05-11 01:54:11 +0800

    bruh

--- web/app/env.mjs
+++ web/app/env.mjs
@@ -5,13 +5,10 @@ export const env = createEnv({
   server: {
     TWITCH_CLIENT_ID: z.string(),
     TWITCH_CLIENT_SECRET: z.string(),
-    TWITCH_REDIRECT_URL: z.string(),
   },
   client: {},
   runtimeEnv: {
-    TWITCH_CLIENT_ID: process.env.TWITCH_CLIENT_ID,
-    TWITCH_CLIENT_SECRET: process.env.TWITCH_CLIENT_SECRET,
-    TWITCH_REDIRECT_URL:
-      process.env.TWITCH_REDIRECT_URL ?? "http://localhost:3000",
+    TWITCH_CLIENT_ID: process.env.NEXT_PUBLIC_TWITCH_CLIENT_ID,
+    TWITCH_CLIENT_SECRET: process.env.NEXT_PUBLIC_TWITCH_CLIENT_SECRET,
   },
 });
