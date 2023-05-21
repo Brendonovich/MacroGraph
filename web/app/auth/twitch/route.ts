2943fb0e5da4dc07f0cbe59648fc215f8abd6e1a
Brendan Allan <brendonovich@outlook.com>
2023-05-21 18:49:31 +0800

    POST refresh endpoint

--- web/app/auth/twitch/route.ts
+++ web/app/auth/twitch/route.ts
@@ -29,3 +29,23 @@ export const GET = async (req: NextRequest) => {
     `http://localhost:${params.state.port}?access_token=${token.access_token}`
   );
 };
+
+export const POST = async (req: NextRequest) => {
+  const body = await req.json();
+
+  const res = await fetch("https://id.twitch.tv/oauth2/token", {
+    method: "POST",
+    body: new URLSearchParams({
+      client_id: env.TWITCH_CLIENT_ID,
+      client_secret: env.TWITCH_CLIENT_SECRET,
+      grant_type: "refresh_token",
+      refresh_token: body.refreshToken,
+    }),
+  });
+
+  const json = await res.json();
+
+  const token = TOKEN.parse(json);
+
+  return NextResponse.json(token);
+};
