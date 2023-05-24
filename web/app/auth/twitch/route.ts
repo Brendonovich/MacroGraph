6b2101ddc6f2479e11d6324321e59f5d9437a528
Brendan Allan <brendonovich@outlook.com>
2023-05-24 09:27:03 +0800

    object

--- web/app/auth/twitch/route.ts
+++ web/app/auth/twitch/route.ts
@@ -66,10 +66,13 @@ export const POST = async (req: NextRequest) => {
 };
 
 export const OPTIONS = () =>
-  NextResponse.json(undefined, {
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-      "Access-Control-Allow-Headers": "Content-Type, Authorization",
-    },
-  });
+  NextResponse.json(
+    {},
+    {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      },
+    }
+  );
