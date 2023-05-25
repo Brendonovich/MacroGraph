7e1d8e62699ee0f7c54f7d51ffaa0ea7e908ca97
jdude700 <info@jdude.tv>
2023-05-25 17:33:51 +1000

    added styling on account logins

--- app/src/settings/Twitch.tsx
+++ app/src/settings/Twitch.tsx
@@ -6,81 +6,98 @@ import { rspc } from "~/rspc";
 
 export default () => {
   const [loggingIn, setLoggingIn] = createSignal(false);
-
   const { helix, chat, auth } = twitch;
 
   return (
     <>
-      <table class="table-auto">
-        <thead>
-          <tr>
-            <th>Account</th>
-            <th>Helix Api</th>
-            <th>Chat Account</th>
-            <th>Chat Channel</th>
-          </tr>
-        </thead>
-        <tbody>
-          <For each={Object.values(twitch.auth.tokens)}>
-            {(token) => (
-              <tr>
-                <td>{token.userName}</td>
-                <td>
-                  <input
-                    type="radio"
-                    id="helix"
-                    checked={twitch.helix
-                      .userId()
-                      .map((id) => id === token.userId)
-                      .unwrapOr(false)}
-                    onChange={(r) => {
-                      if (r.target.checked) helix.setUserId(Some(token.userId));
-                    }}
-                  />
-                </td>
-                <td>
-                  <input
-                    type="radio"
-                    id="Chat Read"
-                    checked={chat
-                      .readUserId()
-                      .map((u) => u === token.userId)
-                      .unwrapOr(false)}
-                    onChange={(r) => {
-                      if (r.target.checked)
-                        chat.setReadUserId(Some(token.userId));
-                    }}
-                  />
-                </td>
-                <td>
-                  <input
-                    type="radio"
-                    id="Chat Write"
-                    checked={chat
-                      .writeUserId()
-                      .map((u) => u === token.userId)
-                      .unwrapOr(false)}
-                    onChange={(r) => {
-                      if (r.target.checked)
-                        chat.setWriteUserId(Some(token.userId));
-                    }}
-                  />
-                </td>
-              </tr>
-            )}
-          </For>
-        </tbody>
-      </table>
       <Switch
         fallback={
           <>
-            {/* Loading...
-            <Button onClick={() => twitch.auth.authProvider.logOut()}>
-              Cancel
-            </Button> */}
+            <Button onClick={() => setLoggingIn(true)}>Login</Button>
           </>
         }
       >
+        <Match when={Object.keys(auth.tokens).length !== 0}>
+          <table class="mb-2 table-auto">
+            <thead>
+              <tr>
+                <th class="pr-2 text-left">Account</th>
+                <th class="px-2 text-center content-center align-middle">
+                  Helix Api
+                </th>
+                <th class="px-2 text-center content-center align-middle">
+                  Chat Account
+                </th>
+                <th class="px-2 text-center content-center align-middle">
+                  Chat Channel
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <For each={Object.values(twitch.auth.tokens)}>
+                {(token) => (
+                  <tr>
+                    <td>{token.userName}</td>
+                    <td class="text-center content-center align-middle">
+                      <input
+                        type="radio"
+                        id="helix"
+                        checked={twitch.helix
+                          .userId()
+                          .map((id) => id === token.userId)
+                          .unwrapOr(false)}
+                        onChange={(r) => {
+                          if (r.target.checked)
+                            helix.setUserId(Some(token.userId));
+                        }}
+                      />
+                    </td>
+                    <td class="text-center content-center align-middle">
+                      <input
+                        type="radio"
+                        id="Chat Read"
+                        checked={chat
+                          .readUserId()
+                          .map((u) => u === token.userId)
+                          .unwrapOr(false)}
+                        onChange={(r) => {
+                          if (r.target.checked)
+                            chat.setReadUserId(Some(token.userId));
+                        }}
+                      />
+                    </td>
+                    <td class="text-center content-center align-middle">
+                      <input
+                        type="radio"
+                        id="Chat Write"
+                        checked={chat
+                          .writeUserId()
+                          .map((u) => u === token.userId)
+                          .unwrapOr(false)}
+                        onChange={(r) => {
+                          if (r.target.checked)
+                            chat.setWriteUserId(Some(token.userId));
+                        }}
+                      />
+                    </td>
+                    <td>
+                      {" "}
+                      <Button
+                        onClick={() => {
+                          auth.logOut(token.userId);
+                        }}
+                      >
+                        Remove
+                      </Button>
+                    </td>
+                  </tr>
+                )}
+              </For>
+            </tbody>
+          </table>
+        </Match>
+      </Switch>
+      <Switch>
         <Match when={loggingIn()}>
           {(_) => {
             rspc.createSubscription(() => ["auth.twitch"], {
@@ -108,8 +125,10 @@ export default () => {
             );
           }}
         </Match>
+        <Match when={!loggingIn()}>
+          <Button onClick={() => setLoggingIn(true)}>Add Account</Button>
+        </Match>
       </Switch>
-      <Button onClick={() => setLoggingIn(true)}>Login</Button>
     </>
   );
 };
