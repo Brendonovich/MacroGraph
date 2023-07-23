9c14d64f06b391ed2105e486397f6ef34d06906b
Hannah-GBS <HannahRyanster@gmail.com>
2023-07-23 09:44:40 +0100

    finish Discord io conversion

--- packages/src/discord/api.ts
+++ packages/src/discord/api.ts
@@ -57,31 +57,30 @@ pkg.createNonEventSchema({
   name: "Send Discord Message",
   variant: "Exec",
   generateIO: (io) => {
-    io.execInput({
-      id: "exec",
-    });
-    io.dataInput({
-      id: "message",
-      name: "Message",
-      type: t.string(),
-    });
-    io.dataInput({
-      id: "channelId",
-      name: "Channel ID",
-      type: t.string(),
-    });
-    io.dataInput({
-      id: "everyone",
-      name: "Allow @Everyone",
-      type: t.bool(),
-    });
+    return {
+      message: io.dataInput({
+        id: "message",
+        name: "Message",
+        type: t.string(),
+      }),
+      channelId: io.dataInput({
+        id: "channelId",
+        name: "Channel ID",
+        type: t.string(),
+      }),
+      everyone: io.dataInput({
+        id: "everyone",
+        name: "Allow @Everyone",
+        type: t.bool(),
+      }),
+    };
   },
-  async run({ ctx }) {
-    await api.channels(ctx.getInput("channelId")).messages.post(z.any(), {
+  async run({ ctx, io }) {
+    await api.channels(ctx.getInput(io.channelId)).messages.post(z.any(), {
       body: {
-        content: ctx.getInput("message"),
+        content: ctx.getInput(io.message),
         allowed_mentions: {
-          parse: ctx.getInput("everyone") ? ["everyone"] : [],
+          parse: ctx.getInput(io.everyone) ? ["everyone"] : [],
         },
       },
     });
@@ -128,58 +127,60 @@ pkg.createNonEventSchema({
   name: "Get Discord Guild User",
   variant: "Exec",
   generateIO: (io) => {
-    io.dataInput({
-      id: "guildId",
-      name: "Guild ID",
-      type: t.string(),
-    });
-    io.dataInput({
-      id: "userId",
-      name: "User ID",
-      type: t.string(),
-    });
-    io.dataOutput({
-      id: "username",
-      name: "UserName",
-      type: t.string(),
-    });
-    io.dataOutput({
-      id: "displayName",
-      name: "Display Name",
-      type: t.string(),
-    });
-    io.dataOutput({
-      id: "avatarId",
-      name: "Avatar ID",
-      type: t.string(),
-    });
-    io.dataOutput({
-      id: "bannerId",
-      name: "Banner ID",
-      type: t.string(),
-    });
-    io.dataOutput({
-      id: "nick",
-      name: "Nickname",
-      type: t.string(),
-    });
-    io.dataOutput({
-      id: "roles",
-      name: "Roles",
-      type: t.list(t.string()),
-    });
+    return {
+      guildId: io.dataInput({
+        id: "guildId",
+        name: "Guild ID",
+        type: t.string(),
+      }),
+      userId: io.dataInput({
+        id: "userId",
+        name: "User ID",
+        type: t.string(),
+      }),
+      username: io.dataOutput({
+        id: "username",
+        name: "UserName",
+        type: t.option(t.string()),
+      }),
+      displayName: io.dataOutput({
+        id: "displayName",
+        name: "Display Name",
+        type: t.option(t.string()),
+      }),
+      avatarId: io.dataOutput({
+        id: "avatarId",
+        name: "Avatar ID",
+        type: t.option(t.string()),
+      }),
+      bannerId: io.dataOutput({
+        id: "bannerId",
+        name: "Banner ID",
+        type: t.option(t.string()),
+      }),
+      nick: io.dataOutput({
+        id: "nick",
+        name: "Nickname",
+        type: t.option(t.string()),
+      }),
+      roles: io.dataOutput({
+        id: "roles",
+        name: "Roles",
+        type: t.list(t.string()),
+      }),
+    };
   },
-  async run({ ctx }) {
+  async run({ ctx, io }) {
     const response = await api
-      .guilds(ctx.getInput("guildId"))
-      .member(ctx.getInput("userId"))
+      .guilds(ctx.getInput(io.guildId))
+      .member(ctx.getInput(io.userId))
       .get(GUILD_MEMBER_SCHEMA);
 
-    ctx.setOutput("username", Maybe(response.user?.username));
-    ctx.setOutput("avatarId", Maybe(response.user?.avatar));
-    ctx.setOutput("bannerId", Maybe(response.user?.banner));
-    ctx.setOutput("nick", response.nick);
-    ctx.setOutput("roles", response.roles);
+    ctx.setOutput(io.username, Maybe(response.user?.username));
+    ctx.setOutput(io.avatarId, Maybe(response.user?.avatar));
+    ctx.setOutput(io.bannerId, Maybe(response.user?.banner));
+    ctx.setOutput(io.nick, Maybe(response.nick));
+    ctx.setOutput(io.roles, response.roles);
   },
 });
 
@@ -187,58 +188,60 @@ pkg.createNonEventSchema({
   name: "Get Discord Role By Id",
   variant: "Exec",
   generateIO: (io) => {
-    io.dataInput({
-      id: "guildId",
-      name: "Guild ID",
-      type: t.string(),
-    });
-    io.dataInput({
-      id: "roleIdIn",
-      name: "Role ID",
-      type: t.string(),
-    });
-    io.dataOutput({
-      id: "name",
-      name: "Name",
-      type: t.string(),
-    });
-    io.dataOutput({
-      id: "roleIdOut",
-      name: "Role ID",
-      type: t.string(),
-    });
-    io.dataOutput({
-      id: "position",
-      name: "Position",
-      type: t.int(),
-    });
-    io.dataOutput({
-      id: "mentionable",
-      name: "Mentionable",
-      type: t.bool(),
-    });
-    io.dataOutput({
-      id: "permissions",
-      name: "Permissions",
-      type: t.bool(),
-    });
+    return {
+      guildId: io.dataInput({
+        id: "guildId",
+        name: "Guild ID",
+        type: t.string(),
+      }),
+      roleIdIn: io.dataInput({
+        id: "roleIdIn",
+        name: "Role ID",
+        type: t.string(),
+      }),
+      name: io.dataOutput({
+        id: "name",
+        name: "Name",
+        type: t.string(),
+      }),
+      roleIdOut: io.dataOutput({
+        id: "roleIdOut",
+        name: "Role ID",
+        type: t.string(),
+      }),
+      position: io.dataOutput({
+        id: "position",
+        name: "Position",
+        type: t.int(),
+      }),
+      mentionable: io.dataOutput({
+        id: "mentionable",
+        name: "Mentionable",
+        type: t.bool(),
+      }),
+      permissions: io.dataOutput({
+        id: "permissions",
+        name: "Permissions",
+        type: t.string(),
+      }),
+    };
   },
-  async run({ ctx }) {
-    let roleId = ctx.getInput("roleIdIn");
+  async run({ ctx, io }) {
+    let roleId = ctx.getInput(io.roleIdIn);
 
     const roles = await api
-      .guilds(ctx.getInput("guildId"))
+      .guilds(ctx.getInput(io.guildId))
       .roles.get(z.array(ROLE_SCHEMA));
 
     const role = roles.find((role) => role.id === roleId);
 
     if (!role) return;
 
-    ctx.setOutput("name", role.name);
-    ctx.setOutput("roleIdOut", role.id);
-    ctx.setOutput("position", role.position);
-    ctx.setOutput("mentionable", role.mentionable);
-    ctx.setOutput("permissions", role.permissions);
+    ctx.setOutput(io.name, role.name);
+    ctx.setOutput(io.roleIdOut, role.id);
+    ctx.setOutput(io.position, role.position);
+    ctx.setOutput(io.mentionable, role.mentionable);
+    ctx.setOutput(io.permissions, role.permissions);
   },
 });
 
@@ -246,48 +249,51 @@ pkg.createNonEventSchema({
   name: "Send Discord Webhook",
   variant: "Exec",
   generateIO: (io) => {
-    io.dataInput({
-      id: "webhookUrl",
-      name: "Webhook URL",
-      type: t.string(),
-    });
-    io.dataInput({
-      id: "content",
-      name: "Message",
-      type: t.string(),
-    });
-    io.dataInput({
-      id: "username",
-      name: "Username",
-      type: t.string(),
-    });
-    io.dataInput({
-      id: "avatarUrl",
-      name: "Avatar URL",
-      type: t.string(),
-    });
-    io.dataInput({
-      id: "tts",
-      name: "TTS",
-      type: t.bool(),
-    });
-    //io.dataInput({
-    //   id: "fileLocation",
-    //   name: "File Location",
-    //   type: types.option(types.string()),
-    // });
-    io.dataOutput({
-      id: "status",
-      name: "Status",
-      type: t.int(),
-    });
+    return {
+      webhookUrl: io.dataInput({
+        id: "webhookUrl",
+        name: "Webhook URL",
+        type: t.string(),
+      }),
+      content: io.dataInput({
+        id: "content",
+        name: "Message",
+        type: t.string(),
+      }),
+      username: io.dataInput({
+        id: "username",
+        name: "Username",
+        type: t.string(),
+      }),
+      avatarUrl: io.dataInput({
+        id: "avatarUrl",
+        name: "Avatar URL",
+        type: t.string(),
+      }),
+      tts: io.dataInput({
+        id: "tts",
+        name: "TTS",
+        type: t.bool(),
+      }),
+      // fileLocation: io.dataInput({
+      //   id: "fileLocation",
+      //   name: "File Location",
+      //   type: types.option(types.string()),
+      // }),
+      status: io.dataOutput({
+        id: "status",
+        name: "Status",
+        type: t.int(),
+      }),
+    };
   },
-  async run({ ctx }) {
+  async run({ ctx, io }) {
     const body: Record<string, string> = {};
-    if (ctx.getInput("content")) body.content = ctx.getInput("content");
-    if (ctx.getInput("avatarUrl")) body.avatar_url = ctx.getInput("avatarUrl");
-    if (ctx.getInput("username")) body.username = ctx.getInput("username");
-    if (ctx.getInput("tts")) body.tts = ctx.getInput<boolean>("tts").toString();
+    if (ctx.getInput(io.content)) body.content = ctx.getInput(io.content);
+    if (ctx.getInput(io.avatarUrl))
+      body.avatar_url = ctx.getInput(io.avatarUrl);
+    if (ctx.getInput(io.username)) body.username = ctx.getInput(io.username);
+    if (ctx.getInput(io.tts)) body.tts = ctx.getInput(io.tts).toString();
     // ctx.getInput<Option<string>>("content").map((v) => (body.content = v));
     // ctx.getInput<Option<string>>("avatarUrl").map((v) => (body.avatar_url = v));
     // ctx.getInput<Option<string>>("username").map((v) => (body.username = v));
@@ -305,7 +311,7 @@ pkg.createNonEventSchema({
     let response = await rspcClient.query([
       "http.json",
       {
-        url: ctx.getInput("webhookUrl"),
+        url: ctx.getInput(io.webhookUrl),
         method: "POST",
         body: {
           Form: body,
@@ -313,6 +319,6 @@ pkg.createNonEventSchema({
       },
     ]);
 
-    ctx.setOutput("status", response.status);
+    ctx.setOutput(io.status, response.status);
   },
 });
