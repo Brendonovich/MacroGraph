903d5a6255e393404e7fb62ed79e0161e2075b8d
Brendan Allan <brendonovich@outlook.com>
2023-09-22 03:03:39 +0800

    delete print button

--- interface/src/components/PrintOutput.tsx
+++ interface/src/components/PrintOutput.tsx
@@ -1,4 +1,6 @@
 import { createSignal, For, onCleanup, onMount } from "solid-js";
+import { AiOutlineDelete } from "solid-icons/ai";
+
 import { useCore } from "../contexts";
 
 export const PrintOutput = () => {
@@ -19,7 +21,10 @@ export const PrintOutput = () => {
   return (
     <div class="flex-1 flex flex-col overflow-y-hidden">
       <div class="flex flex-row bg-neutral-900 text-white px-2 font-medium shadow py-1">
-        Print Output
+        <span class="flex-1">Print Output</span>
+        <button onClick={() => setItems([])}>
+          <AiOutlineDelete />
+        </button>
       </div>
       <ul class="p-1 gap-y-2 flex flex-col flex-1 overflow-y-auto ">
         <For each={items()}>
