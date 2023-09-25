becea027836b8bec5e0162945efaa586517a7fcf
jdudetv <info@jdude.tv>
2023-09-25 15:49:39 +1000

    Made it so you can zoom out more (#188)

--- interface/src/UIStore.tsx
+++ interface/src/UIStore.tsx
@@ -107,7 +107,7 @@ export function createUIStore(core: Core) {
     },
     updateScale(delta: number, screenOrigin: XY) {
       const startGraphOrigin = this.toGraphSpace(screenOrigin);
-      state.scale = Math.min(Math.max(0.5, state.scale + delta / 10), 2.5);
+      state.scale = Math.min(Math.max(0.25, state.scale + delta / 10), 2.5);
       const endGraphOrigin = this.toScreenSpace(startGraphOrigin);
 
       state.translate = {
