867f4f6c60face3225611100cf637b7b27a769e8
jdude700 <info@jdude.tv>
2023-11-19 15:20:54 +1000

    show node string inputs as editble text areas in the sidebar

--- interface/src/Sidebars.tsx
+++ interface/src/Sidebars.tsx
@@ -1,5 +1,7 @@
-import { Graph, Node } from "@macrograph/core";
+import { DataInput as DataInputModel, Graph, Node } from "@macrograph/core";
 import { SidebarSection } from "./components/Sidebar";
+import { For, Match, Show, Switch } from "solid-js";
+import { TextArea } from "@kobalte/core/dist/types/text-field";
 
 export function GraphSidebar(_: { graph: Graph }) {
   return (
@@ -13,7 +15,37 @@ export function NodeSidebar(props: { node: Node }) {
   return (
     <>
       <SidebarSection title="Node Properties">
-        <p>Name: {props.node.state.name}</p>
+        <div class="pl-1 text-white">
+          <p>Name: {props.node.state.name}</p>
+          <p>
+            position: {Math.floor(props.node.state.position.x)},{" "}
+            {Math.floor(props.node.state.position.y)}
+          </p>
+
+          <For each={props.node.state.inputs}>
+            {(i) => (
+              <Switch>
+                <Match when={i instanceof DataInputModel ? i : null}>
+                  {(i) => (
+                    <p class="pr-2">
+                      {i().id}:{" "}
+                      {typeof i().defaultValue === "string" ? (
+                        <textarea
+                          wrap="hard"
+                          value={i().defaultValue?.toString()}
+                          onchange={(e) => i().setDefaultValue(e.target.value)}
+                          class="flex flex-row items-center w-full border border-gray-300 rounded bg-black focus:border-yellow-500 focus:ring-0 "
+                        ></textarea>
+                      ) : (
+                        <p>{i().defaultValue}</p>
+                      )}
+                    </p>
+                  )}
+                </Match>
+              </Switch>
+            )}
+          </For>
+        </div>
       </SidebarSection>
     </>
   );
