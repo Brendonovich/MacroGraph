d6de4a46f954ba2509a49db7c2936ea3e86a02ac
SkechyWolf <37350695+SkechyWolf@users.noreply.github.com>
2024-01-23 15:54:02 -0800

    zindex fix

--- interface/src/settings/ui.tsx
+++ interface/src/settings/ui.tsx
@@ -44,15 +44,15 @@ export const DialogRoot = (
       <KobalteDialog.Portal mount={ctx.rootRef()}>
         <KobalteDialog.Overlay
           class={clsx(
-            "absolute inset-0 bg-black/50 backdrop-blur-sm focus:outline-none z-50",
+            "absolute inset-0 bg-black/50 backdrop-blur-sm focus:outline-none",
             "ui-expanded:animate-in ui-expanded:fade-in-0",
             "ui-not-expanded:animate-out ui-expanded:fade-out-0"
           )}
         />
-        <div class="absolute inset-0 z-50 flex flex-col justify-center items-center">
+        <div class="absolute inset-0 flex flex-col justify-center items-center">
           <KobalteDialog.Content
             class={clsx(
-              "flex-1 flex flex-col items-center justify-center duration-200 overflow-hidden p-8 z-50",
+              "flex-1 flex flex-col items-center justify-center duration-200 overflow-hidden p-8",
               "ui-expanded:animate-in ui-expanded:fade-in-0 ui-expanded:zoom-in-[0.98]",
               "ui-not-expanded:animate-out ui-not-expanded:fade-out-0 ui-not-expanded:zoom-out-[0.98]"
               // "ui-not-expanded:slide-out-to-left-1/2 ui-not-expanded:slide-out-to-top-[48%] ui-expanded:slide-in-from-left-1/2 ui-expanded:slide-in-from-top-[48%]"
