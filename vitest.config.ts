6cabd0bd99aa2b499ff281ff49595d6acc1e9885
Brendan Allan <brendonovich@outlook.com>
2024-01-25 12:35:02 +0800

    test-in-ci

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,13 @@
 import { defineConfig } from "vitest/config";
 import solid from "vite-plugin-solid";
+import GithubActionsReporter from "vitest-github-actions-reporter";
 
 export default defineConfig({
   plugins: [solid()],
+
+  test: {
+    reporters: process.env.GITHUB_ACTIONS
+      ? ["default", new GithubActionsReporter()]
+      : undefined,
+  },
 });
