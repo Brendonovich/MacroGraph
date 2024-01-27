1e681a906518e0227fa6a79b0b31029c050fb8bb
Brendan Allan <brendonovich@outlook.com>
2024-01-27 20:10:06 +0800

    use catchError

--- packages/runtime/src/models/Node.ts
+++ packages/runtime/src/models/Node.ts
@@ -195,25 +195,19 @@ export class Node extends Disposable {
         const s = this.schema;
 
         createEffect(() => {
-          onCleanup(() => console.log("what the bruh"));
-          try {
-            const unsub = s
-              .createListener({
-                properties: s.properties ?? {},
-                ctx: {
-                  getProperty: (p) => this.getProperty(p) as any,
-                  graph: this.graph,
-                },
-              })
-              .listen((data) => new ExecutionContext(this).run(data));
-
-            onCleanup(unsub);
-          } catch {}
-          // catchError(
-          //   () => {
-          //   },
-          //   (e) => console.error(e)
-          // );
+          catchError(() => {
+            onCleanup(
+              s
+                .createListener({
+                  properties: s.properties ?? {},
+                  ctx: {
+                    getProperty: (p) => this.getProperty(p) as any,
+                    graph: this.graph,
+                  },
+                })
+                .listen((data) => new ExecutionContext(this).run(data))
+            );
+          }, console.error);
         });
       }
     });
