8ee4d6ecb08f8d9cb45729a6fc824703a494cc45
Brendan Allan <brendonovich@outlook.com>
2024-07-09 20:33:06 +0800

    async main

--- .github/createTag.js
+++ .github/createTag.js
@@ -1,18 +1,20 @@
 const tagRef = "tags/${{ steps.read_version.outputs.value }}";
 
-const tag = await github.rest.git.getTag({
-	ref: tagRef,
-	owner: context.repo.owner,
-	repo: context.repo.repo,
-});
-
-const newTag = tag !== undefined;
-core.setOutput("new_tag", newTag);
-
-if (!newTag)
-	await github.rest.git.createRef({
-		ref: "refs/${tagRef}",
+async function main() {
+	const tag = await github.rest.git.getTag({
+		ref: tagRef,
 		owner: context.repo.owner,
 		repo: context.repo.repo,
-		sha: context.sha,
 	});
+
+	const newTag = tag !== undefined;
+	core.setOutput("new_tag", newTag);
+
+	if (!newTag)
+		await github.rest.git.createRef({
+			ref: "refs/${tagRef}",
+			owner: context.repo.owner,
+			repo: context.repo.repo,
+			sha: context.sha,
+		});
+}
