d50e71d39a77b12a20cadd574af22bc068af0ef8
Brendan Allan <brendonovich@outlook.com>
2024-07-24 02:37:35 +0800

    unify input styles

--- interface/src/components/ui/FloatInput.tsx
+++ interface/src/components/ui/FloatInput.tsx
@@ -1,58 +1,56 @@
 import clsx from "clsx";
 import { createEffect, createSignal } from "solid-js";
+import { Input } from "./Input";
 
 interface Props {
-	initialValue: number;
-	onChange(v: number): void;
-	class?: string;
-	value?: number;
+  initialValue: number;
+  onChange(v: number): void;
+  class?: string;
+  value?: number;
 }
 
 export const FloatInput = (props: Props) => {
-	// if NaN reset to 0
-	const initialValue = Number.isNaN(props.initialValue)
-		? 0
-		: props.initialValue;
-	props.onChange(initialValue);
+  // if NaN reset to 0
+  const initialValue = Number.isNaN(props.initialValue)
+    ? 0
+    : props.initialValue;
+  props.onChange(initialValue);
 
-	const [value, setValue] = createSignal(props.initialValue.toString());
-	createEffect(() => {
-		if (props.value !== undefined) setValue(props.value.toString());
-	});
+  const [value, setValue] = createSignal(props.initialValue.toString());
+  createEffect(() => {
+    if (props.value !== undefined) setValue(props.value.toString());
+  });
 
-	return (
-		<input
-			type="text"
-			value={value()}
-			onKeyDown={(e) => e.stopPropagation()}
-			onKeyUp={(e) => e.stopPropagation()}
-			onChange={(e) => {
-				e.stopPropagation();
+  return (
+    <Input
+      type="text"
+      value={value()}
+      onKeyDown={(e) => e.stopPropagation()}
+      onKeyUp={(e) => e.stopPropagation()}
+      onChange={(e) => {
+        e.stopPropagation();
 
-				const value = e.target.value;
-				setValue(value);
+        const value = e.target.value;
+        setValue(value);
 
-				const numValue = Number.parseFloat(value);
-				if (!Number.isNaN(numValue)) props.onChange(numValue);
-			}}
-			onBlur={(e) => {
-				const s = e.target.value;
-				const num = Number.parseFloat(s);
+        const numValue = Number.parseFloat(value);
+        if (!Number.isNaN(numValue)) props.onChange(numValue);
+      }}
+      onBlur={(e) => {
+        const s = e.target.value;
+        const num = Number.parseFloat(s);
 
-				if (s.length === 0) {
-					setValue("0");
-					props.onChange(0);
-				} else if (Number.isNaN(num)) {
-					setValue(props.initialValue.toString());
-				} else {
-					setValue(num.toString());
-					props.onChange(num);
-				}
-			}}
-			class={clsx(
-				"w-full text-xs h-5 px-1 border border-neutral-500 rounded bg-black focus:border-yellow-500 focus:ring-0",
-				props.class,
-			)}
-		/>
-	);
+        if (s.length === 0) {
+          setValue("0");
+          props.onChange(0);
+        } else if (Number.isNaN(num)) {
+          setValue(props.initialValue.toString());
+        } else {
+          setValue(num.toString());
+          props.onChange(num);
+        }
+      }}
+      class={props.class}
+    />
+  );
 };
