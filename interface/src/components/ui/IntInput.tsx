5e72be6bc6f7b9d13433c2e9d4fe5815f4cbb7b9
Brendan Allan <brendonovich@outlook.com>
2024-07-24 15:21:27 +0800

    fix int input

--- interface/src/components/ui/IntInput.tsx
+++ interface/src/components/ui/IntInput.tsx
@@ -1,7 +1,9 @@
 import clsx from "clsx";
-import { batch, createEffect, createSignal } from "solid-js";
+import { batch, createEffect, createSignal, onMount } from "solid-js";
+import { NumberField } from "@kobalte/core/number-field";
 
 import { Input } from "./Input";
+import { createEventListener } from "@solid-primitives/event-listener";
 
 interface Props {
   initialValue: number;
@@ -17,75 +19,73 @@ export const IntInput = (props: Props) => {
     : props.initialValue;
   props.onChange(initialValue);
 
-  createEffect(() => {
-    if (props.value !== undefined) setValue(props.value.toString());
-  });
+  // createEffect(() => {
+  //   if (props.value !== undefined) setValue(props.value.toString());
+  // });
 
   const [value, setValue] = createSignal(initialValue.toString());
+  const [rawValue, setRawValue] = createSignal(initialValue);
 
   let ref: HTMLInputElement;
 
+  onMount(() => {
+    createEventListener(ref, "wheel", () => {});
+  });
+
   return (
-    <div class="relative">
-      <Input
+    <NumberField
+      class="relative"
+      value={value()}
+      onChange={setValue}
+      rawValue={rawValue()}
+      onRawValueChange={setRawValue}
+      changeOnWheel
+      step={1}
+    >
+      <NumberField.Input<typeof Input>
         ref={ref!}
-        type="text"
-        value={value()}
-        onKeyDown={(e) => e.stopPropagation()}
-        onKeyUp={(e) => e.stopPropagation()}
-        onChange={(e) => {
-          const value = e.target.value;
+        as={(inputProps) => (
+          <Input
+            {...inputProps}
+            onBlur={() => {
+              batch(() => {
+                if (value() === "") {
+                  setValue("0");
+                  setRawValue(0);
+                } else if (Number.isNaN(rawValue())) {
+                  setValue(props.initialValue.toString());
+                  setRawValue(props.initialValue);
+                }
 
-          setValue(value);
-
-          const numValue = Number.parseInt(value);
-          if (!Number.isNaN(numValue)) props.onChange(Number.parseInt(value));
-        }}
-        onBlur={(e) => {
-          const s = e.target.value;
-          const num = Number.parseInt(s);
-
-          if (s.length === 0) {
-            setValue("0");
-            props.onChange(0);
-          } else if (Number.isNaN(num)) {
-            setValue(props.initialValue.toString());
-          } else {
-            setValue(num.toString());
-            props.onChange(num);
-          }
-        }}
-        class={clsx("pr-4", props.class)}
+                props.onChange(rawValue());
+              });
+            }}
+            onKeyDown={(e) => e.stopPropagation()}
+            onKeyUp={(e) => e.stopPropagation()}
+            class={clsx("pr-4", props.class)}
+          />
+        )}
       />
       <div class="absolute right-0 top-0 flex flex-col justify-between bottom-0 p-1 gap-px">
-        <button
+        <NumberField.IncrementTrigger
           type="button"
           class="bg-white/20 flex items-center justify-center rounded-t-sm focus-visible:outline-yellow-400 focus-visible:outline"
           onClick={() => {
-            batch(() => {
-              const val = (props.value ?? 0) + 1;
-              console.log({ val });
-              props.onChange(val);
-              setValue(val.toString());
-            });
+            queueMicrotask(() => props.onChange(rawValue()));
           }}
         >
           <IconMaterialSymbolsArrowDropUpRounded class="h-4 -my-1" />
-        </button>
-        <button
+        </NumberField.IncrementTrigger>
+        <NumberField.DecrementTrigger
           type="button"
           class="bg-white/20 flex items-center justify-center rounded-b-sm focus-visible:outline-yellow-400 focus-visible:outline"
           onClick={() => {
-            batch(() => {
-              const val = (props.value ?? 0) - 1;
-              props.onChange(val);
-              setValue(val.toString());
-            });
+            queueMicrotask(() => props.onChange(rawValue()));
           }}
         >
           <IconMaterialSymbolsArrowDropDownRounded class="h-4 -my-1" />
-        </button>
+        </NumberField.DecrementTrigger>
       </div>
-    </div>
+    </NumberField>
   );
 };
