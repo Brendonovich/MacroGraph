3ce7145547fb64cac48ff204e24b15826d4a3553
Brendan Allan <brendonovich@outlook.com>
2024-08-12 16:19:15 +0800

    jest-dom

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,10 +2,10 @@ import solid from "vite-plugin-solid";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-	plugins: [solid()],
-	// test: {
-	// 	reporters: process.env.GITHUB_ACTIONS
-	// 		? ["default", new GithubActionsReporter()]
-	// 		: [],
-	// },
+  plugins: [solid()],
+  test: {
+    reporters: process.env.GITHUB_ACTIONS
+      ? ["default", new GithubActionsReporter()]
+      : [],
+  },
 });
