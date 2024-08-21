6c1737927aee5787e8a6ef98eb93148f82a7c3df
Brendan Allan <brendonovich@outlook.com>
2024-08-21 12:57:37 +0800

    pass api to desktop core
    closes #469

--- apps/desktop/src/core.ts
+++ apps/desktop/src/core.ts
@@ -1,10 +1,11 @@
 import {
-	Core,
-	type RefreshedOAuthToken,
-	createWsProvider,
+  Core,
+  type RefreshedOAuthToken,
+  createWsProvider,
 } from "@macrograph/runtime";
 import "tauri-plugin-midi";
 
+import { rawApi } from "./api";
 import { env } from "./env";
 import { fetch } from "./http";
 import { client } from "./rspc";
@@ -12,47 +13,48 @@ import { client } from "./rspc";
 const AUTH_URL = `${env.VITE_MACROGRAPH_API_URL}/auth`;
 
 export const core = new Core({
-	fetch: fetch as any,
-	oauth: {
-		authorize: (provider) =>
-			new Promise((res) => {
-				client.addSubscription(
-					["oauth.authorize", `${AUTH_URL}/${provider}/login`],
-					{
-						onData(data) {
-							res({ ...data, issued_at: Date.now() / 1000 });
-						},
-					},
-				);
-			}),
-		refresh: async (provider, refreshToken) => {
-			const res = await fetch(`${AUTH_URL}/${provider}/refresh`, {
-				method: "POST",
-				headers: { "content-type": "application/json" },
-				body: JSON.stringify({ refreshToken }),
-			});
+  fetch: fetch as any,
+  api: rawApi,
+  oauth: {
+    authorize: (provider) =>
+      new Promise((res) => {
+        client.addSubscription(
+          ["oauth.authorize", `${AUTH_URL}/${provider}/login`],
+          {
+            onData(data) {
+              res({ ...data, issued_at: Date.now() / 1000 });
+            },
+          },
+        );
+      }),
+    refresh: async (provider, refreshToken) => {
+      const res = await fetch(`${AUTH_URL}/${provider}/refresh`, {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify({ refreshToken }),
+      });
 
-			return {
-				...((await res.json()) as RefreshedOAuthToken),
-				issued_at: Date.now() / 1000,
-			};
-		},
-	},
+      return {
+        ...((await res.json()) as RefreshedOAuthToken),
+        issued_at: Date.now() / 1000,
+      };
+    },
+  },
 });
 
-const wsProvider = createWsProvider({
-	async startServer(port, onData) {
-		return client.addSubscription(["websocket.server", port], {
-			onData: (d) => onData(d),
-		});
-	},
-	async stopServer(unsubscribe) {
-		unsubscribe();
-	},
-	async sendMessage(data) {
-		return client.mutation([
-			"websocket.send",
-			{ port: data.port, client: data.client, data: data.data },
-		]);
-	},
+export const wsProvider = createWsProvider({
+  async startServer(port, onData) {
+    return client.addSubscription(["websocket.server", port], {
+      onData: (d) => onData(d),
+    });
+  },
+  async stopServer(unsubscribe) {
+    unsubscribe();
+  },
+  async sendMessage(data) {
+    return client.mutation([
+      "websocket.send",
+      { port: data.port, client: data.client, data: data.data },
+    ]);
+  },
 });
