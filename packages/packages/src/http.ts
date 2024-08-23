49bc7869e780a47cddb7ab8acf41df3b5214e6da
Hannah_GBS <7504779+Hannah-GBS@users.noreply.github.com>
2024-08-23 04:32:59 +0100

    Add GET File node and update HTTP nodes with additional inputs and outputs (#446)
    * Add GET File node and update HTTP nodes with additional inputs and outputs
    
    * fixes

--- packages/packages/src/http.ts
+++ packages/packages/src/http.ts
@@ -1,8 +1,33 @@
-import { type Core, Package } from "@macrograph/runtime";
-import { t } from "@macrograph/typesystem";
+import { JSONEnum, jsonToJS, type JSONValue, jsToJSON } from "@macrograph/json";
+import { Maybe, None } from "@macrograph/option";
+import { type Core, Package, type PropertyDef } from "@macrograph/runtime";
+import { type MapValue, t } from "@macrograph/typesystem";
+import { ReactiveMap } from "@solid-primitives/map";
+import { writeBinaryFile } from "@tauri-apps/api/fs";
+
+export async function streamToArrayBuffer(
+	stream: ReadableStream<Uint8Array>,
+): Promise<Uint8Array> {
+	return new Uint8Array(await new Response(stream).arrayBuffer());
+}
 
 export function pkg(core: Core) {
 	const pkg = new Package({ name: "HTTP Requests" });
+
+	const BodyEnum = pkg.createEnum("Body", (e) => [
+		e.variant("Plaintext", {
+			value: t.string(),
+		}),
+		e.variant("HTML", {
+			value: t.string(),
+		}),
+		e.variant("JSON", {
+			value: t.enum(JSONEnum),
+		}),
+		e.variant("FormData", {
+			value: t.map(t.string()),
+		}),
+	]);
 
 	pkg.createSchema({
 		name: "GET",
@@ -14,26 +39,198 @@ export function pkg(core: Core) {
 					name: "URL",
 					type: t.string(),
 				}),
-				response: io.dataOutput({
-					id: "response",
-					name: "Response",
-					type: t.string(),
+				headers: io.dataInput({
+					id: "headers",
+					name: "Headers",
+					type: t.map(t.string()),
+				}),
+				responseBody: io.dataOutput({
+					id: "responseBody",
+					name: "Response Body",
+					type: t.option(t.enum(BodyEnum)),
 				}),
 				status: io.dataOutput({
 					id: "status",
-					name: "Status",
+					name: "Response Status",
 					type: t.int(),
+				}),
+				responseHeaders: io.dataOutput({
+					id: "responseHeaders",
+					name: "Response Headers",
+					type: t.map(t.string()),
 				}),
 			};
 		},
 		async run({ ctx, io }) {
+			const headers: Record<string, string> = { ...ctx.getInput(io.headers) };
 			const response = await core.fetch(ctx.getInput(io.url), {
 				method: "GET",
+				headers,
 			});
 
-			// TODO: Change when Objects implemented
-			ctx.setOutput(io.response, await response.text());
+			ctx.setOutput(
+				io.responseBody,
+				Maybe(
+					await (async () => {
+						switch (response.headers.get("Content-Type")!.split(";")[0]) {
+							case "text/plain": {
+								return BodyEnum.variant([
+									"Plaintext",
+									{
+										value: await response.text(),
+									},
+								]);
+							}
+							case "text/html": {
+								return BodyEnum.variant([
+									"HTML",
+									{
+										value: await response.text(),
+									},
+								]);
+							}
+							case "application/json": {
+								return BodyEnum.variant([
+									"JSON",
+									{
+										value: jsToJSON(await response.json())!,
+									},
+								]);
+							}
+							case "multipart/form-data": {
+								const formData = new ReactiveMap<string, string>();
+								for (const entry of (await response.formData()).entries()) {
+									formData.set(entry[0], entry[1].toString());
+								}
+								return BodyEnum.variant([
+									"FormData",
+									{
+										value: formData,
+									},
+								]);
+							}
+							default: {
+								return null;
+							}
+						}
+					})(),
+				),
+			);
+
 			ctx.setOutput(io.status, response.status);
+			ctx.setOutput(
+				io.responseHeaders,
+				new ReactiveMap<string, string>(response.headers.entries()),
+			);
+		},
+	});
+
+	pkg.createSchema({
+		name: "GET File",
+		type: "exec",
+		createIO({ io }) {
+			return {
+				url: io.dataInput({
+					id: "url",
+					name: "URL",
+					type: t.string(),
+				}),
+				path: io.dataInput({
+					id: "path",
+					name: "File Path",
+					type: t.string(),
+				}),
+				headers: io.dataInput({
+					id: "headers",
+					name: "Headers",
+					type: t.map(t.string()),
+				}),
+				responseBody: io.dataOutput({
+					id: "responseBody",
+					name: "Response Body",
+					type: t.option(t.enum(BodyEnum)),
+				}),
+				status: io.dataOutput({
+					id: "status",
+					name: "Response Status",
+					type: t.int(),
+				}),
+				responseHeaders: io.dataOutput({
+					id: "responseHeaders",
+					name: "Response Headers",
+					type: t.map(t.string()),
+				}),
+			};
+		},
+		async run({ ctx, io }) {
+			const headers: Record<string, string> = { ...ctx.getInput(io.headers) };
+			const response = await core.fetch(ctx.getInput(io.url), {
+				method: "GET",
+				headers,
+			});
+
+			if (response.body && response.status === 200) {
+				const data = await streamToArrayBuffer(response.body);
+				console.log("array buffer created");
+				writeBinaryFile(ctx.getInput(io.path), data);
+				console.log("file written");
+				ctx.setOutput(io.responseBody, None);
+			} else {
+				ctx.setOutput(
+					io.responseBody,
+					Maybe(
+						await (async () => {
+							switch (response.headers.get("Content-Type")) {
+								case "text/plain": {
+									return BodyEnum.variant([
+										"Plaintext",
+										{
+											value: await response.text(),
+										},
+									]);
+								}
+								case "text/html": {
+									return BodyEnum.variant([
+										"HTML",
+										{
+											value: await response.text(),
+										},
+									]);
+								}
+								case "application/json": {
+									return BodyEnum.variant([
+										"JSON",
+										{
+											value: jsToJSON(await response.json())!,
+										},
+									]);
+								}
+								case "multipart/form-data": {
+									const formData = new ReactiveMap<string, string>();
+									for (const entry of (await response.formData()).entries()) {
+										formData.set(entry[0], entry[1].toString());
+									}
+									return BodyEnum.variant([
+										"FormData",
+										{
+											value: formData,
+										},
+									]);
+								}
+								default: {
+									return null;
+								}
+							}
+						})(),
+					),
+				);
+			}
+
+			ctx.setOutput(io.status, response.status);
+			ctx.setOutput(
+				io.responseHeaders,
+				new ReactiveMap<string, string>(response.headers.entries()),
+			);
 		},
 	});
 
@@ -50,32 +247,132 @@ export function pkg(core: Core) {
 				body: io.dataInput({
 					id: "body",
 					name: "Body",
-					type: t.string(),
+					type: t.enum(BodyEnum),
 				}),
-				response: io.dataOutput({
-					id: "response",
-					name: "Response",
-					type: t.string(),
+				headers: io.dataInput({
+					id: "headers",
+					name: "Headers",
+					type: t.map(t.string()),
+				}),
+				responseBody: io.dataOutput({
+					id: "responseBody",
+					name: "Response Body",
+					type: t.option(t.enum(BodyEnum)),
 				}),
 				status: io.dataOutput({
 					id: "status",
-					name: "Status",
+					name: "Response Status",
 					type: t.int(),
+				}),
+				responseHeaders: io.dataOutput({
+					id: "responseHeaders",
+					name: "Response Headers",
+					type: t.map(t.string()),
 				}),
 			};
 		},
 		async run({ ctx, io }) {
+			const headers = new Headers();
+
+			for (const [key, value] of Object.entries(ctx.getInput(io.headers))) {
+				headers.set(key.toLowerCase(), value);
+			}
+
+			let type: string;
+			let body: string | FormData | null;
+			const input = ctx.getInput(io.body);
+			switch (input.variant) {
+				case "Plaintext": {
+					type = "text/plain";
+					body = input.data.value;
+					break;
+				}
+				case "JSON": {
+					type = "application/json";
+					body = window.JSON.stringify(jsonToJS(input.data.value));
+					break;
+				}
+				case "FormData": {
+					type = "multipart/form-data";
+					const fd = new FormData();
+					input.data.value.forEach((value, key) => {
+						fd.append(key, value);
+					});
+
+					body = fd;
+					break;
+				}
+				case "HTML": {
+					type = "text/html";
+					body = input.data.value;
+					break;
+				}
+			}
+
+			if (type) {
+				headers.set("content-type", type);
+			}
+
 			const response = await core.fetch(ctx.getInput(io.url), {
 				method: "POST",
-				body: JSON.stringify(ctx.getInput(io.body)),
-				headers: {
-					"content-type": "application/json; charset=UTF-8",
-				},
+				body,
+				headers,
 			});
 
-			// TODO: Change when Objects implemented
-			ctx.setOutput(io.response, await response.text());
+			ctx.setOutput(
+				io.responseBody,
+				Maybe(
+					await (async () => {
+						switch (response.headers.get("Content-Type")!.split(";")[0]) {
+							case "text/plain": {
+								return BodyEnum.variant([
+									"Plaintext",
+									{
+										value: await response.text(),
+									},
+								]);
+							}
+							case "text/html": {
+								return BodyEnum.variant([
+									"HTML",
+									{
+										value: await response.text(),
+									},
+								]);
+							}
+							case "application/json": {
+								return BodyEnum.variant([
+									"JSON",
+									{
+										value: jsToJSON(await response.json())!,
+									},
+								]);
+							}
+							case "multipart/form-data": {
+								const formData = new ReactiveMap<string, string>();
+								for (const entry of (await response.formData()).entries()) {
+									formData.set(entry[0], entry[1].toString());
+								}
+								return BodyEnum.variant([
+									"FormData",
+									{
+										value: formData,
+									},
+								]);
+							}
+							default: {
+								return null;
+							}
+						}
+					})(),
+				),
+			);
+
 			ctx.setOutput(io.status, response.status);
+			ctx.setOutput(
+				io.responseHeaders,
+				new ReactiveMap<string, string>(response.headers.entries()),
+			);
 		},
 	});
 
@@ -92,32 +389,130 @@ export function pkg(core: Core) {
 				body: io.dataInput({
 					id: "body",
 					name: "Body",
-					type: t.string(),
+					type: t.enum(BodyEnum),
 				}),
-				response: io.dataOutput({
-					id: "response",
-					name: "Response",
-					type: t.string(),
+				headers: io.dataInput({
+					id: "headers",
+					name: "Headers",
+					type: t.map(t.string()),
+				}),
+				responseBody: io.dataOutput({
+					id: "responseBody",
+					name: "Response Body",
+					type: t.option(t.enum(BodyEnum)),
 				}),
 				status: io.dataOutput({
 					id: "status",
-					name: "Status",
+					name: "Response Status",
 					type: t.int(),
+				}),
+				responseHeaders: io.dataOutput({
+					id: "responseHeaders",
+					name: "Response Headers",
+					type: t.map(t.string()),
 				}),
 			};
 		},
 		async run({ ctx, io }) {
+			const headers = new Headers();
+
+			for (const [key, value] of Object.entries(ctx.getInput(io.headers))) {
+				headers.set(key.toLowerCase(), value);
+			}
+
+			let type: string;
+			let body: string | FormData | null;
+			const input = ctx.getInput(io.body);
+			switch (input.variant) {
+				case "Plaintext": {
+					type = "text/plain";
+					body = input.data.value;
+					break;
+				}
+				case "JSON": {
+					type = "application/json";
+					body = window.JSON.stringify(jsonToJS(input.data.value));
+					break;
+				}
+				case "FormData": {
+					type = "multipart/form-data";
+					const fd = new FormData();
+					input.data.value.forEach((value, key) => {
+						fd.append(key, value);
+					});
+
+					body = fd;
+					break;
+				}
+				case "HTML": {
+					type = "text/html";
+					body = input.data.value;
+					break;
+				}
+			}
+
+			headers.set("content-type", type);
+
 			const response = await core.fetch(ctx.getInput(io.url), {
 				method: "PUT",
-				body: JSON.stringify(ctx.getInput(io.body)),
-				headers: {
-					"content-type": "application/json; charset=UTF-8",
-				},
+				body,
+				headers,
 			});
 
-			// TODO: Change when Objects implemented
-			ctx.setOutput(io.response, await response.text());
+			ctx.setOutput(
+				io.responseBody,
+				Maybe(
+					await (async () => {
+						switch (response.headers.get("Content-Type")!.split(";")[0]) {
+							case "text/plain": {
+								return BodyEnum.variant([
+									"Plaintext",
+									{
+										value: await response.text(),
+									},
+								]);
+							}
+							case "text/html": {
+								return BodyEnum.variant([
+									"HTML",
+									{
+										value: await response.text(),
+									},
+								]);
+							}
+							case "application/json": {
+								return BodyEnum.variant([
+									"JSON",
+									{
+										value: jsToJSON(await response.json())!,
+									},
+								]);
+							}
+							case "multipart/form-data": {
+								const formData = new ReactiveMap<string, string>();
+								for (const entry of (await response.formData()).entries()) {
+									formData.set(entry[0], entry[1].toString());
+								}
+								return BodyEnum.variant([
+									"FormData",
+									{
+										value: formData,
+									},
+								]);
+							}
+							default: {
+								return null;
+							}
+						}
+					})(),
+				),
+			);
+
 			ctx.setOutput(io.status, response.status);
+			ctx.setOutput(
+				io.responseHeaders,
+				new ReactiveMap<string, string>(response.headers.entries()),
+			);
 		},
 	});
 
@@ -131,26 +526,129 @@ export function pkg(core: Core) {
 					name: "URL",
 					type: t.string(),
 				}),
-				response: io.dataOutput({
-					id: "response",
-					name: "Response",
-					type: t.string(),
+				headers: io.dataInput({
+					id: "headers",
+					name: "Headers",
+					type: t.map(t.string()),
 				}),
 				status: io.dataOutput({
 					id: "status",
-					name: "Status",
+					name: "Response Status",
 					type: t.int(),
+				}),
+				responseHeaders: io.dataOutput({
+					id: "responseHeaders",
+					name: "Response Headers",
+					type: t.map(t.string()),
 				}),
 			};
 		},
 		async run({ ctx, io }) {
+			const headers: Record<string, string> = { ...ctx.getInput(io.headers) };
 			const response = await core.fetch(ctx.getInput(io.url), {
 				method: "DELETE",
+				headers,
 			});
 
-			// TODO: Change when Objects implemented
-			ctx.setOutput(io.response, await response.text());
 			ctx.setOutput(io.status, response.status);
+			ctx.setOutput(
+				io.responseHeaders,
+				new ReactiveMap<string, string>(response.headers.entries()),
+			);
+		},
+	});
+
+	pkg.createSchema({
+		name: "URL Encode Component",
+		type: "pure",
+		createIO({ io }) {
+			return {
+				input: io.dataInput({
+					id: "input",
+					type: t.string(),
+				}),
+				output: io.dataOutput({
+					id: "output",
+					type: t.string(),
+				}),
+			};
+		},
+		run({ ctx, io }) {
+			ctx.setOutput(io.output, encodeURIComponent(ctx.getInput(io.input)));
+		},
+	});
+
+	const bodyTypeProperty = {
+		name: "Body Type",
+		source: () =>
+			BodyEnum.variants.map((value) => ({
+				id: value.name,
+				display: value.name,
+			})),
+	} satisfies PropertyDef;
+
+	pkg.createSchema({
+		name: "Create Body",
+		type: "pure",
+		properties: {
+			bodyType: bodyTypeProperty,
+		},
+		createIO({ ctx, io, properties }) {
+			const bodyTypeProp = ctx.getProperty(properties.bodyType)!;
+
+			const bodyType = () => {
+				switch (bodyTypeProp) {
+					case "Plaintext":
+					case "HTML":
+						return t.string();
+					case "FormData":
+						return t.map(t.string());
+					case "JSON":
+						return t.enum(JSONEnum);
+				}
+			};
+			return {
+				input: io.dataInput({
+					id: "input",
+					type: bodyType(),
+				}),
+				output: io.dataOutput({
+					id: "output",
+					type: t.enum(BodyEnum),
+				}),
+			};
+		},
+		run({ ctx, io, properties }) {
+			const output = () => {
+				const bodyType = ctx.getProperty(properties.bodyType)!;
+				switch (bodyType) {
+					case "Plaintext": {
+						return BodyEnum.variant([
+							"Plaintext",
+							{ value: ctx.getInput(io.input) as string },
+						]);
+					}
+					case "HTML": {
+						return BodyEnum.variant([
+							"HTML",
+							{ value: ctx.getInput(io.input) as string },
+						]);
+					}
+					case "JSON": {
+						return BodyEnum.variant([
+							"JSON",
+							{ value: ctx.getInput(io.input) as JSONValue },
+						]);
+					}
+					case "FormData": {
+						return BodyEnum.variant([
+							"FormData",
+							{ value: ctx.getInput(io.input) as MapValue<string> },
+						]);
+					}
+				}
+			};
+			ctx.setOutput(io.output, output());
 		},
 	});
 
