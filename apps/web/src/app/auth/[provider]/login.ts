15eb15512033f258344dba2eeafdcb8c3d6c08cd
Brendan Allan <brendonovich@outlook.com>
2024-08-23 14:24:18 +0800

    idk

--- apps/web/src/app/auth/[provider]/login.ts
+++ apps/web/src/app/auth/[provider]/login.ts
@@ -5,16 +5,18 @@ import { getOAuthLoginURL } from "../actions";
 import { AuthProviders } from "../providers";
 
 export const GET = async (event: APIEvent) => {
-	const { provider } = event.params as { provider: string };
+  const { provider } = event.params as { provider: string };
 
-	const providerConfig = AuthProviders[provider];
-	if (!providerConfig) throw new Error(`Unknown provider ${provider}`);
+  const providerConfig = AuthProviders[provider];
+  if (!providerConfig) throw new Error(`Unknown provider ${provider}`);
 
-	const url = new URL(event.request.url);
+  const url = new URL(event.request.url);
 
-	const stateFromSearchParam = JSON.parse(
-		Buffer.from(url.searchParams.get("state")!, "base64").toString(),
-	) as any;
+  const stateFromSearchParam = JSON.parse(
+    Buffer.from(url.searchParams.get("state")!, "base64").toString(),
+  ) as any;
 
-	return redirect(await getOAuthLoginURL(provider, stateFromSearchParam));
+  return redirect(
+    await getOAuthLoginURL(provider, url.origin, stateFromSearchParam),
+  );
 };
