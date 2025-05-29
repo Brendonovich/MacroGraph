98ab3d5b1152b2593a3a2ae9e84111559b571c2f
Brendan Allan <brendonovich@outlook.com>
2025-05-29 22:49:50 +0800

    posthog client init

--- apps/web/src/entry-client.tsx
+++ apps/web/src/entry-client.tsx
@@ -1,4 +1,10 @@
 import { StartClient, mount } from "@solidjs/start/client";
 import "./posthog/client";
+import posthog from "posthog-js";
 
 mount(() => <StartClient />, document.getElementById("app")!);
+
+posthog.init("phc_7anSDyS3p1frzGL7bHWlkiNG8kJ9pxcHB8H7QjBMEMB", {
+  api_host: "https://us.i.posthog.com",
+  person_profiles: "identified_only",
+});
--- apps/web/src/posthog/client.ts
+++
@@ -1,9 +0,0 @@
-import posthog from "posthog-js";
-
-export const posthogClient = posthog.init(
-  "phc_7anSDyS3p1frzGL7bHWlkiNG8kJ9pxcHB8H7QjBMEMB",
-  {
-    api_host: "https://us.i.posthog.com",
-    person_profiles: "identified_only",
-  },
-);
