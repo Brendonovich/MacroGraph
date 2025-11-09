fe6b53d8e7982e7982412994643ffc321167f98b
Brendan Allan <brendonovich@outlook.com>
2025-11-09 16:31:48 +0800

    update SS

--- apps/web/src/entry-server.tsx
+++ apps/web/src/entry-server.tsx
@@ -13,7 +13,6 @@ export default createHandler(
               name="viewport"
               content="width=device-width, initial-scale=1, maximum-scale=1"
             />
-
             {/*<link rel="stylesheet" href={globalCss} />*/}
             {assets}
           </head>
